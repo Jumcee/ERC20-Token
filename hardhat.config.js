96ef4d88a60bb7a343e0f3f6c1518df51fbf6771
Jumcee <futhmah456@gmail.com>
2023-11-27 23:17:06 +0100

    ERC20-Token

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,11 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.20",
   networks: {
     goerli: {
       url: 'https://eth-goerli.g.alchemy.com/v2/MTI2hmWU3Ye0S5A72rt2LiEabbLaAI0K', // Update with your Infura project ID
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: ["process.env.PRIVATE_KEY"],
       chainId: 5. // Goerli chain ID
     }
   }
